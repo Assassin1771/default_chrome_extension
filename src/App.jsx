c5cbea6067e2dd40c04208af5a332cd97756d077
Tushar Garg <tushar.garg@procol.in>
2025-02-05 21:09:12 +0530

    base setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Scroll Position</h1>
+      <p>Current scroll position px</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        background: "src/background.js",
+        content: "src/content.js",
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+  define: {
+    // This ensures chrome is treated as a global variable
+    chrome: "chrome",
+  },
+});
